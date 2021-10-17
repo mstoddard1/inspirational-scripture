d897821b5e2863295acd92b09bb574fbf5e3fce2
Mary Stoddard <mstoddard319@gmail.com>
2021-10-16 21:12:07 -0600

    file is not working right now.

--- randomscript/src/Main.java
+++ randomscript/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Main {
+    String[] scriptures = {"24 And I said unto them that it was the word of God; and whoso would hearken unto the word " +
+            "of God, and would hold fast unto it, they would never perish; neither could the temptations and the fiery" +
+            " darts of the adversary overpower them unto blindness, to lead them away to destruction."};
+    public static void printScript() {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
     public static void main(String[] args) {
+
+        // Marianne's code goes here -->
+
 
     }
 }
