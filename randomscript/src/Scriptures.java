e8194c236ab3b20d16077b574a87b338de2ef1b6
isaiahmstoddard <i.stoddardhome@gmail.com>
2021-10-27 20:26:13 -0600

    Added more scriptures to the _Scriptures_ class.

--- randomscript/src/Scriptures.java
+++ randomscript/src/Scriptures.java
@@ -7,7 +7,13 @@ public class Scriptures {
             "for ye are laying the foundation of a great work. And out of small things proceedeth that which is great. - D&C 64:33",
             "Behold, I am God; give heed to my word, which is quick and powerful, sharper than a two-edged sword, to the " +
                     "dividing asunder of both joints and marrow; therefore, give heed unto my word. - D&C 12:2",
-            "Therefore, if you will ask of me you shall receive; if you will knock it shall be opened unto you. - D&C 12:5"};
+            "Therefore, if you will ask of me you shall receive; if you will knock it shall be opened unto you. - D&C 12:5",
+            "If my people, which are called by my name, shall humble themselves, and pray, and seek my face, and turn from " +
+                    "their wicked ways; then will I hear from heaven, and will forgive their sin, and will heal their land. - 2 Chronicles 7:14",
+            "Fear not: for they that be with us are more than they that be with them. - 2 Kings 6:16",
+            "For God hath not given us the spirit of fear; but of power, and of love, and of a sound mind. - 2 Timothy 1:7",
+            "The Son of Man hath descended below them all. Art thou greater than he? - D&C 122:8",
+            "Wherefore, be of good cheer, and do not fear, for I the Lord am with you, and will stand by you. D&C 68:6"};
     public static int length = scriptures.length + 1;
 
 }
